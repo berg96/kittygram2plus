5d47b62783bc4177554269ff16db2c6f8c7a2e37
Artem Kulikov <chigar2010@yandex.ru>
2023-12-09 00:51:48 +0200

    add pagination

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -143,6 +143,9 @@ REST_FRAMEWORK = {
         'anon': '1000/day',  # Лимит для AnonRateThrottle
         'low_request': '1/minute',
     },
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
 SIMPLE_JWT = {
